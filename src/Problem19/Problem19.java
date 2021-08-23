85b7b434b81773ba59019983f597a3eda6eb79d3
ThomasCanning <thomas500book@gmail.com>
2021-08-23 17:27:33 +0100

    problem 19

--- src/Problem19/Problem19.java
+++ src/Problem19/Problem19.java
@@ -1,4 +1,78 @@
 package Problem19;
 
 public class Problem19 {
+
+    public static void main(String[] args) {
+        int firstSundays = 0;
+        int jan = 2;
+        int feb = 5;
+        int march = 5;
+        int april = 1;
+        int may = 3;
+        int june = 6;
+        int july = 1;
+        int august = 4;
+        int september = 6;
+        int october = 2;
+        int november = 5;
+        int december = 6;
+        int day = 2;
+        for(int year = 1901;year <=2000;year++) {
+
+            if (year%4==0) {
+                switch (day) {
+                    case 1:
+                        firstSundays = firstSundays + 2;
+                        break;
+                    case 2:
+                        firstSundays = firstSundays + 1;
+                        break;
+                    case 3:
+                        firstSundays = firstSundays + 2;
+                        break;
+                    case 4:
+                        firstSundays = firstSundays + 2;
+                        break;
+                    case 5:
+                        firstSundays = firstSundays + 1;
+                        break;
+                    case 6:
+                        firstSundays = firstSundays + 1;
+                        break;
+                    case 7:
+                        firstSundays = firstSundays + 3;
+                        break;
+                }
+                if (day <7) day++; else day = 1;
+            }
+            else {
+                switch (day) {
+                    case 1:
+                        firstSundays = firstSundays + 2;
+                        break;
+                    case 2:
+                        firstSundays = firstSundays + 2;
+                        break;
+                    case 3:
+                        firstSundays = firstSundays + 1;
+                        break;
+                    case 4:
+                        firstSundays = firstSundays + 3;
+                        break;
+                    case 5:
+                        firstSundays = firstSundays + 1;
+                        break;
+                    case 6:
+                        firstSundays = firstSundays + 1;
+                        break;
+                    case 7:
+                        firstSundays = firstSundays + 2;
+                        break;
+                }
+                if (day <7) day++; else day = 1;
+            }
+        }
+        System.out.println(firstSundays);
+
+    }
 }
